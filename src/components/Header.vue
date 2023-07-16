681f198604720f50a966d976945e797b771e2113
Dan Bush <dan@knows.email>
2023-07-15 22:46:27 -0500

    Added header component

---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+defineProps<{
+  msg: string
+}>()
+</script>
+
+<template>
+  <header class="main-header">
+    {{msg}}
+  </header>
+</template>
+
+<style scoped lang="scss">
+header.main-header {
+  width: 100%;
+  height: 50px;
+  background-color: $background-light;
+  color: black;
+  text-align: center;
+  font-family: "BitterPro", 'Courier New', Courier, monospace;
+  font-weight: 900;
+  font-size: 2rem;
+  color: $foreground-primary;
+}
+</style>
