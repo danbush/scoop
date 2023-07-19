cde0ecfadd70a79c7baa10a344803d1975ea6028
Dan Bush <dan@knows.email>
2023-07-19 09:22:37 -0500

    removed required props in footer

--- src/components/CardFooter.vue
+++ src/components/CardFooter.vue
@@ -5,9 +5,6 @@
     - fill out this section
     - pass in props from parent (until then, URLS are dummies)
   ***************************/
-  defineProps<{
-    url: string
-  }>()
 </script>
 
 <template>
