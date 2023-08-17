6a267ec54c2363c1b367ccb4045c37c94d0c43e3
stephenmparke <stephenmparke@gmail.com>
2023-08-17 12:02:02 -0500

    Missed a changed file
    Whoops.

--- src/components/GodBar/_ThemeSwitcher.vue
+++ src/components/GodBar/_ThemeSwitcher.vue
@@ -4,6 +4,19 @@
   todo: fill out this section
   ***************************/
 
+  /*  Dark Mode toggle
+  https://www.vuemastery.com/blog/implementing-dark-mode-with-vueuse/
+
+  For future reference (additional themes:)
+  https://vueuse.org/core/useColorMode/
+
+  */
+  import { useDark, useToggle } from "@vueuse/core";
+  const isDark = useDark({
+    selector: "html",
+  });
+  const toggleDark = useToggle(isDark);
+
   function toggleThemeSupport() {
     document.body.classList.toggle('devmode-themesupport')
   }
@@ -14,10 +27,12 @@
 
   <div class="toggle toggle_theme">
     <p class="toggle-label">Theme</p>
+
     <label class="switch">
-      <input type="checkbox" v-on:click="toggleThemeSupport" />
+      <input type="checkbox" @click="toggleDark()" />
       <span class="slider"></span>
     </label>
+
   </div>
 
 </template>
