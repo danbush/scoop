09e4c8b61152d10e66af7249299f6bcabddc6dc1
stephenmparke <stephenmparke@gmail.com>
2023-09-03 09:15:50 -0500

    Ensure elements exist for modal/keyboard shortcuts

--- src/components/helpers/getPipedEmbed.ts
+++ src/components/helpers/getPipedEmbed.ts
@@ -1,8 +1,8 @@
-export async function getPipedEmbed(originalUrl) {
+export async function getPipedEmbed(originalUrl: string) {
 	// Extract video ID from the original URL using regex
 	const regex = /watch\?v=([^&]+)/;
 	const matches = originalUrl.match(regex);
-	console.log(originalUrl)
+	console.log('sven! originalUrl: ' + originalUrl)
 	if (matches && matches[1]) {
 		const videoId = matches[1];
 		const embeddedUrl = `https://piped.scoopy.zone/embed/${videoId}`;
