635755a91c14044b7afd487a2ab4d16cb5e5a0a2
Dan Bush <dan@knows.email>
2023-09-03 15:45:27 -0500

    Removed errors from waffle cone

--- src/components/helpers/waffle_cone.ts
+++ src/components/helpers/waffle_cone.ts
@@ -6,20 +6,24 @@ tags: filter dedupe
 *****************************/
 
 // imports
-import { chocolateSauce } from './chocolate_sauce'
 import { hashtagBuildTheList } from './hashtag_buildthelist'
 import { listVideos } from './list_videos'
 import { listMastodon } from './list_mastodon'
 import { getRandomNumbersInRange } from './sprinkle_getRandomNumbersInRange'
 
 // set some defaults
-var article_source_quantity: number = hashtagBuildTheList().length
-var video_source_quantity: number = listVideos().length
-var mastodon_source_quantity: number = listMastodon().length
+const hashtagList = hashtagBuildTheList();
+var article_source_quantity: number = hashtagList ? hashtagList.length : 0;
 
-const used_article_set: Set<number> = new Set();
-const used_video_set: Set<number> = new Set();
-const used_social_set: Set<number> = new Set();
+const videoList = listVideos();
+var video_source_quantity: number = videoList ? videoList.length : 0;
+
+const mastodonList = listMastodon();
+var mastodon_source_quantity: number = mastodonList ? mastodonList.length : 0;
+
+let used_article_set: Set<number> = new Set();
+let used_video_set: Set<number> = new Set();
+let used_social_set: Set<number> = new Set();
 
 export enum ContentSource {
 	Articles,
@@ -42,19 +46,19 @@ export function waffleCone(source: ContentSource, count: number = 24) {
 	// Use a switch statement to handle different sources
 	switch (source) {
 			case ContentSource.Articles:
-				source_numbers = getRandomNumbersInRange(count, 0, ContentSourceQuantity.Articles - 1);
-				used_article_set.add(...source_numbers);
-				
+				source_numbers = getRandomNumbersInRange(count, 0, ContentSourceQuantity.Articles - 1)  as number[];
+				used_article_set = new Set([...Array.from(used_article_set), ...(source_numbers as number[])]);
+
 				source_numbers.forEach((num) => {
 					if (num >= 0 && num < ContentSourceQuantity.Articles) {
 						article_source_quantity--;
 					}
 				});
 				return source_numbers;
-				break;
+
 			case ContentSource.Videos:
-					source_numbers = getRandomNumbersInRange(count, 0, ContentSourceQuantity.Videos - 1);
-					used_video_set.add(...source_numbers);
+					source_numbers = getRandomNumbersInRange(count, 0, ContentSourceQuantity.Videos - 1) as number[];
+					used_video_set = new Set([...Array.from(used_video_set), ...(source_numbers as number[])]);
 					
 					source_numbers.forEach((num) => {
 						if (num >= 0 && num < ContentSourceQuantity.Videos) {
@@ -62,18 +66,18 @@ export function waffleCone(source: ContentSource, count: number = 24) {
 						}
 					});
 					return source_numbers;
-					break;
+	
 			case ContentSource.Social:
-					source_numbers = getRandomNumbersInRange(count, 0, ContentSourceQuantity.Social - 1);
-					used_social_set.add(...source_numbers);
+					source_numbers = getRandomNumbersInRange(count, 0, ContentSourceQuantity.Social - 1) as number[];
+					used_social_set = new Set([...Array.from(used_social_set), ...(source_numbers as number[])]);
 					
 					source_numbers.forEach((num) => {
 						if (num >= 0 && num < ContentSourceQuantity.Social) {
-							social_source_quantity--;
+							mastodon_source_quantity--;
 						}
 					});
 					return source_numbers;
-					break;
+
 			default:
 					throw new Error('Invalid content source');
 	}
